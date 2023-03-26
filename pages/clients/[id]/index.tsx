f3cd62177fffc1751a190d1085bf2779c86f1866
김진영 <jinyoung01099@gmail.com>
2023-03-26 21:31:46 +0900

    [Add] Programming 방식을 통한 Navigating

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -5,9 +5,16 @@ const ClientsProjectPage = () => {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load data
+    router.push("/client/max/projecta");
+    // push에는 Link의 href와 똑같이 객체 형태로도 전달 가능
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
