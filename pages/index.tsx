8a8d562851b63067fc1c8cb4c8a8e009841c58ce
김진영 <jinyoung01099@gmail.com>
2023-03-26 21:17:25 +0900

    [Add] 동적 Route로 Navigating

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ const HomePage = () => {
       <ul>
         <li>
           <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
           <Link href="/clients">Clients</Link>
         </li>
       </ul>
